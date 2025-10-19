47befacb21c15056ef6972e2b5387a4829364c76
xiayipeng <yipeng.xia@hotmail.com>
2025-10-20 03:05:26 +0800

    feat: update 'go to sleep' pose joint trajectory in LiveKitROS2Bridge; adjust position for joint5

--- teleop_livekit/livekit_ros2_bridge.py
+++ teleop_livekit/livekit_ros2_bridge.py
@@ -193,7 +193,7 @@ class LiveKitROS2Bridge(Node):
         self.ready_to_pick_pose.points.append(JointTrajectoryPoint(positions=[1.5707, 0.71, -0.58, 0.0, 1.2, 0.0], time_from_start=MsgDuration(sec=1, nanosec=0)))
         self.go_to_sleep_pose = JointTrajectory()
         self.go_to_sleep_pose.joint_names = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
-        self.go_to_sleep_pose.points.append(JointTrajectoryPoint(positions=[-1.5707, 0.0, 0.0, 0.0, 0.0, 0.0], time_from_start=MsgDuration(sec=1, nanosec=0)))
+        self.go_to_sleep_pose.points.append(JointTrajectoryPoint(positions=[-1.5707, 0.0, 0.0, 0.0, 0.2912, 0.0], time_from_start=MsgDuration(sec=1, nanosec=0)))
         # debug
         self.debug_publisher = self.create_publisher(PoseStamped, '/debug/ee_pose', 10)
         # log init ee pose Transform
